59796d48ccfedda21ffd58a94d185fb92293d04b
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-04-20 16:49:08 +0700

    feat: add eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,68 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'simple-import-sort', 'unused-imports'],
   rules: {
+    // import/export path
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    // see: https://www.npmjs.com/package/eslint-plugin-simple-import-sort
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // ext library & side effect imports
+          ['^@?\\w', '^\\u0000'],
+          // images files
+          ['^.+\\.(gif|jpe?g|tiff?|png|webp|bmp)$'],
+          // {s}css files
+          ['^.+\\.s?css$'],
+          // app path
+          ['^@/'],
+          // relative paths up until 3 level
+          [
+            '^\\./?$',
+            '^\\.(?!/?$)',
+            '^\\.\\./?$',
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\.(?!/?$)',
+            '^\\.\\./\\.\\./\\.\\./?$',
+            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+          ],
+          // other that didnt fit in
+          ['^'],
+        ],
+      },
+    ],
+
+    // see: https://www.npmjs.com/package/eslint-plugin-unused-imports
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': ['off'],
+    'react/react-in-jsx-scope': 'off',
   },
 }
